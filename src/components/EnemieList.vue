cb582c798b0fcd754a8f67fbab7613f2d4123d9f
Sajed Nemli <sajednemli39@gmail.com>
2025-07-21 23:05:10 +0100

    feat: add moveset for each enemie wit hnew GIF data

--- src/components/EnemieList.vue
+++ src/components/EnemieList.vue
@@ -18,6 +18,10 @@ const imageUrl = (fileName: string) => {
   return new URL(`../assets/mobs/${fileName}.png`, import.meta.url).href
 }
 
+const gifUrl = (fileName: string) => {
+  return new URL(`../assets/mobs/mobs-gifs/${fileName}.gif`, import.meta.url).href
+}
+
 const orderedEnemies = computed(() => {
   return [...props.enemies].sort((a: Enemie, b: Enemie) => {
     return a[props.order] > b[props.order] ? 1 : -1
@@ -27,7 +31,7 @@ const orderedEnemies = computed(() => {
 
 <template>
   <div class="enemie-list">
-    <p>Order by {{ order }}</p>
+    <p align="center">Order by {{ order }}</p>
     <transition-group name="mobsList" tag="ul">
       <li v-for="enemie in orderedEnemies" :key="enemie.id">
         <h2>{{ enemie.title }}</h2>
@@ -37,9 +41,24 @@ const orderedEnemies = computed(() => {
             <div class="locs" v-for="(loc, index) in enemie.location" :key="index">
               {{ loc }}
             </div>
-            <p class="base-health">{{ enemie.baseHealth }} DBS</p>
+            <p class="base-health">Base Health: {{ enemie.baseHealth }} DBS</p>
           </div>
-          <img :src="imageUrl(enemie.title)" :alt="enemie.title" width="75" height="75" />
+          <div class="image-container">
+            <img
+              class="static-image"
+              :src="imageUrl(enemie.title)"
+              :alt="enemie.title"
+              width="75"
+              height="75"
+            />
+            <img
+              class="gif-image"
+              :src="gifUrl(enemie.title)"
+              :alt="enemie.title"
+              width="250"
+              height="100"
+            />
+          </div>
         </div>
         <div class="descrip">
           <p>{{ enemie.behavior }}</p>
@@ -70,24 +89,59 @@ const orderedEnemies = computed(() => {
   font-family: HyliaSerif;
 }
 .feature {
+  background: #e5e5cb;
   display: flex;
   align-items: center;
   flex-direction: row;
   justify-content: space-around;
   padding: 15px;
+  border-radius: 5px;
 }
-.feature img {
+.image-container {
+  position: relative;
+  display: inline-block;
+}
+
+.static-image {
   object-fit: contain;
+  transition: transform 0.3s;
+  cursor: pointer;
 }
+
+.gif-image {
+  position: absolute;
+  top: -10px;
+  left: 85px;
+  object-fit: contain;
+  opacity: 0;
+  visibility: hidden;
+  transition:
+    opacity 0.3s,
+    visibility 0.3s;
+  z-index: 10;
+  background-color: #d5cea3;
+  border-radius: 5px;
+  padding: 5px;
+  margin: 5px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+}
+
+.image-container:hover .gif-image {
+  opacity: 1;
+  visibility: visible;
+}
+
+.image-container:hover .static-image {
+  transform: scale(1.2);
+}
+
 .info {
   display: flex;
-  background: #e5e5cb;
   padding: 16px;
   flex-direction: column;
   justify-content: space-around;
   width: 30%;
   height: 100px;
-  border-radius: 5px;
 }
 .info p {
   font-size: large;
@@ -95,12 +149,12 @@ const orderedEnemies = computed(() => {
   margin: 0;
 }
 .locs {
-  margin-left: 5px;
+  /* margin-left: 5px; */
 }
 .base-health {
   color: #1a120b;
   font-weight: bold;
-  margin-top: 10px;
+  align-self: self-end;
 }
 .descrip {
   color: #3c2a21;
